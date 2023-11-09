df7fd134c86913b7bd39be608c5e189b9f04b8b5
Fabricio <you@example.com>
2023-11-09 19:05:49 -0400

    faculdade-safegas-app

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+
+export class HomePage implements OnInit{
+  
+  constructor() {}
+
+    ngOnInit(): void {
+    }
+
+  
+}
